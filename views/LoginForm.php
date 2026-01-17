a8937ce03774d0463c570b9a19035171ebadb62b
safaacht <safaachtaoui@gmail.com>
2026-01-17 19:08:45 +0100

    formulaire du login

--- views/LoginForm.php
+++ views/LoginForm.php
@@ -4,12 +4,23 @@
    <?php require_once '../views/header.php'; ?>
 
 
-    <div id="login" class="page">
-      <div class="form-box">
-        <h2>Connexion ISTICHARA</h2>
-        <input id="email" type="email" placeholder="Email">
-        <input id="pass" type="password" placeholder="Mot de passe">
-        <button class="btn" onclick="login()">Se connecter</button>
-      </div>
+    <div id="login" class="page active">
+    <div class="form-box">
+
+        <h2 class="form-title">Connexion</h2>
+        <p class="form-subtitle">Accédez à votre espace ISTICHARA</p>
+
+        <form onsubmit="login(); return false;">
+            <label>Email</label>
+            <input id="email" type="email" placeholder="exemple@email.com" required>
+
+            <label>Mot de passe</label>
+            <input id="pass" type="password" placeholder="********" required>
+
+            <button class="btn full">Se connecter</button>
+        </form>
+
     </div>
+</div>
+
     <?php require_once '../views/footer.php'; ?>
