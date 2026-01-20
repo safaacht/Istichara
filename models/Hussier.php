2e55eb5fc221d1b3968e29897768fba0621a6119
safaacht <safaachtaoui@gmail.com>
2026-01-20 17:07:41 +0100

    modif du construct et l'heritage d'une nouvelle classe

--- models/Hussier.php
+++ models/Hussier.php
@@ -9,26 +9,11 @@ enum Type:string
     case Execution="execution";
     case Constats="constats";
 }
-class Hussier extends Personne{
-    public function __construct(string $name,
-                                string $email, 
-                                string $phone, 
-                                private int $expYears,
-                                private float $hourlyRate,
-                                private Type $type,
-                                ?int $id = null)
-    {
-        parent::__construct($name, $email, $phone, $id);
-    }
+class Hussier extends Profetionnel{
 
-    public function setExpYears(int $exp_years):void
+    public function __construct(string $name, string $phone, float $hourlyRate, int $expYears, string $document,private Type $type, ?int $id = null)
     {
-        $this->expYears=$exp_years;
-    }
-
-    public function setHourlyRate(float $hourly_rate):void
-    {
-        $this->hourlyRate=$hourly_rate;
+        parent::__construct($name, $phone, $hourlyRate, $expYears, $document, $id);
     }
 
     public function setType(Type $type): void
@@ -36,14 +21,6 @@ class Hussier extends Personne{
         $this->type = $type;
     }
 
-
-    public function getExpYears(): int {
-        return $this->expYears;
-    }
-
-    public function getHourlyRate(): float {
-        return $this->hourlyRate;
-    }
 
     public function getType(): Type {
         return $this->type;
