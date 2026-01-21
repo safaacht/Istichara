527d5af221ebd074984e2adccdf683e253dcc6bc
safaacht <safaachtaoui@gmail.com>
2026-01-21 11:37:55 +0100

    fix db name

--- helper/Database.php
+++ helper/Database.php
@@ -5,7 +5,7 @@ use PDO;
 
 class Database implements DatabaseInterface{
     private static string $host="ep-cold-mountain-ah25jfw1-pooler.c-3.us-east-1.aws.neon.tech";
-    private static string $db_name="Istichara";
+    private static string $db_name="Istichara_tree";
     private static string $user="neondb_owner";
     private static string $mdp="npg_umhYVWbnKC90";
 
@@ -17,6 +17,7 @@ class Database implements DatabaseInterface{
             $dsn='pgsql:host=' . self::$host . ';dbname=' . self::$db_name . ';port=5432' . ';sslmode=require';
             self::$conn= new PDO($dsn,self::$user,self::$mdp,[
                 PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_PERSISTENT => true,
                 PDO::ATTR_DEFAULT_FETCH_MODE=> PDO::FETCH_ASSOC
 
             ]);
