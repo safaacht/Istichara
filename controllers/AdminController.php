2f0c0c201fa4818f908b8afbf911c5a29e07f1b4
safaacht <safaachtaoui@gmail.com>
2026-01-22 23:29:43 +0100

    ontroller admin avec l'insertion dans tab 'demande'

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -6,6 +6,7 @@ use repositories\AvocatRepo;
 use repositories\HussierRepo;
 use repositories\UserRepo;
 use helper\Database;
+use models\User;
 
 class AdminController {
 
@@ -26,9 +27,17 @@ class AdminController {
                 $demande = $demandeRepo->findById($id);
                 if(!$demande) throw new \Exception("Demande not found");
 
+                //creating a user 
+                $userRepo = new UserRepo();
+                $role = !empty($demande['specialization']) ? 'avocat' : 'hussier';
+                $password = password_hash($demande['phone'], PASSWORD_DEFAULT);
+                $newUser = new User($demande['email'], $password, $role);
+                $userId = $userRepo->create($newUser);
+
                 $demandeRepo->updateStatus($id, 'accepted');
 
                 $data = [
+                    'user_id' => $userId,
                     'name' => $demande['name'],
                     'email' => $demande['email'],
                     'phone' => $demande['phone'],
@@ -41,13 +50,15 @@ class AdminController {
                 ];
 
                 $repo = null;
-                if (!empty($demande['specialization'])) {
+                if ($role === 'avocat') {
                     // an avocat
                     $data['specialization'] = $demande['specialization'];
+                    unset($data['type']); // making sure it's clean
                     $repo = new AvocatRepo();
-                } elseif (!empty($demande['type'])) {
+                } elseif ($role === 'hussier') {
                     // an hussier
                     $data['type'] = $demande['type'];
+                    unset($data['specialization']); // making sure it's clean
                     $repo = new HussierRepo();
                 } else {
                     throw new \Exception("Unknown role from data");
