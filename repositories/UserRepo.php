afc4da1ee758f7e6e70c1da9f84b4e7f37352706
Alae <aamimi454@gmail.com>
2026-01-23 09:24:24 +0000

    to fetch last vrs

--- repositories/UserRepo.php
+++ repositories/UserRepo.php
@@ -28,21 +28,6 @@ class UserRepo{
 
     }
 
-    public function createAvocat(User $user){
-        $sql = 'INSERT INTO "user"(email, password, role)
-            VALUES(:email, :password, :role)';
-        $stmt = $this->conn->prepare($sql);
-        $result = $stmt->execute([
-            ':email' => $user->getEmail(),
-            ':password' => $user->getPassword(),
-            ':role' => $user->getRole(),
-        ]);
-        $user_id = $this->conn->lastInsertId();
-        $user->setId($user_id);
-        return $user_id;
-
-    }
-
 }
 
 
